3889cea8d32f648f21ed225a63d0b8057b481295
Aleksei Merezhko <aleksei.merezhko@mail.ru>
2019-02-22 19:18:50 +0200

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    userName: null,
+
+  }
+  componentDidMount(){
+    window.gapi.load('auth2', function() {
+    //Library downloaded
+      window.gapi.auth2.init({
+        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+
+      })
+      .then(() => console.log(`init OK`), () => console.error(`init Error`))
+
+
+    });
+  }
+  signIn = () => {
+    const _authOk = (user) => {
+      console.log(`Auth OK`, );
+      this.setState({ userName: user.getBasicProfile().getName()})
+    }
+    const _authErr = () => {
+      console.error(`Auth Error`)
+    }
+    const googleAuth = window.gapi.auth2.getAuthInstance();
+    googleAuth.signIn(
+      {
+        scope: 'profile email'
+      }
+    ).then(_authOk, _authErr)
+  }
+  signOut = () => {
+    const googleAuth = window.gapi.auth2.getAuthInstance();
+    googleAuth.signOut()
+      .then(
+        () => this.setState({userName: null}),
+        () => console.error(`sign out error`)
+      )
+  }
   render() {
+    const { userName } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <p> Google Sign In</p>
+        {userName && <p>HI, {userName}</p>}  
+        {!userName && <button onClick={this.signIn}> Log In</button>}
+        {userName && <button onClick={this.signOut}> Log Out</button>}
         </header>
       </div>
     );
